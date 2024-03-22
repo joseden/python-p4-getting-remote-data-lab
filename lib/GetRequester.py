a040fb9914a09b4dd729de019a6aa1acd44068e1
Joseph <joseph.dena@student.moringaschool.com>
2024-03-22 10:24:33 +0300

    complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,26 @@
+#!usr/bin/env python
+
 import requests
 import json
+
+
+url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
 
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
-
+       
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        return json.loads(self.get_response_body())
+
+# Instantiate the GetRequester class with the URL
+requester = GetRequester(url)
+
+# Call the load_json method to load and print the JSON data
+json_data = requester.load_json()
+print(json_data)
